ea1ff61cd0b451e044fdea00ac93423194af00f2
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-11-02 11:26:24 +0500

    07

--- List07.py
+++ List07.py
@@ -6,4 +6,10 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    a=0
+    while a<len(list1):
+        if list1[a]==0:
+            list1[a]=False
+        a+=1
+    return list1
+print(main([0,1,1,0]))
